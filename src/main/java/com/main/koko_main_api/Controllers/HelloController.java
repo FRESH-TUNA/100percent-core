dffdfc031d0bd31f8b991f29e8319cdfa0535fe6
lunacircle4 <lunacircle4@gmail.com>
2022-01-06 00:10:06 +0900

    feat: dto test

--- src/main/java/com/main/koko_main_api/Controllers/HelloController.java
+++ src/main/java/com/main/koko_main_api/Controllers/HelloController.java
@@ -1,6 +1,8 @@
 package com.main.koko_main_api.Controllers;
 
+import com.main.koko_main_api.Dtos.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // JSON을 반환하는 컨트롤러
@@ -11,4 +13,11 @@ public class HelloController {
     public String hello() {
         return "Hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
 }
+
