659a65ed37ddae3c3ae5a21551c4c6375d483b94
DONGWON KIM <lunacircle4@likelion.org>
2022-01-11 00:23:08 +0900

    feat: /composers/<id>/musics 테스트 통과

--- src/test/java/com/main/koko_main_api/Repositories/ComposesRepositoryTest.java
+++ src/test/java/com/main/koko_main_api/Repositories/ComposesRepositoryTest.java
@@ -22,6 +22,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 //https://www.baeldung.com/spring-data-rest-relationships
 
+
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ComposesRepositoryTest {
@@ -35,36 +37,35 @@ public class ComposesRepositoryTest {
     @Transactional
     public void composer_to_music_test() throws Exception {
         //endpoints
-        String ROOT_ENDPOINT = "http://localhost:" + port + "/main_api/v1/musics";
+        String ROOT_ENDPOINT = "http://localhost:" + port;
         String COMPOSER_ENDPOINT = ROOT_ENDPOINT + "/composers";
         String MUSIC_ENDPOINT = ROOT_ENDPOINT + "/musics";
-        System.out.println(template.getForObject(ROOT_ENDPOINT, String.class));
 
-        // composer 생성
-//        ComposersSaveRequestDto composer1 = ComposersSaveRequestDto
-//                .builder().name("composer1").build();
-//        ComposersSaveRequestDto composer2 = ComposersSaveRequestDto
-//                .builder().name("composer2").build();
-//        template.postForEntity(COMPOSER_ENDPOINT, composer1, ComposersResponseDto.class);
-//        template.postForEntity(COMPOSER_ENDPOINT, composer2, ComposersResponseDto.class);
-//
-//        // music 생성
-//        MusicsSaveRequestDto music1 = MusicsSaveRequestDto
-//                .builder().title("music1").build();
-//        template.postForEntity(MUSIC_ENDPOINT, music1, MusicsResponseDto.class);
-//
-//        // 연관관계 생성
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add("Content-type", "text/uri-list");
-//        HttpEntity<String> entity = new HttpEntity<>(
-//                COMPOSER_ENDPOINT + "/1\n" + COMPOSER_ENDPOINT + "/2\n", headers);
-//        template.exchange(MUSIC_ENDPOINT + "/1/composers", HttpMethod.PUT, entity, String.class);
-//
-//
-//        // 연관관계 생성 checking
-//        String jsonResponse = template.getForObject(ROOT_ENDPOINT, String.class);
-//        JSONObject jsonObj = new JSONObject(jsonResponse).getJSONObject("_embedded");
-//        JSONArray jsonArray = jsonObj.getJSONArray("musics");
-//        assertThat(jsonArray.getJSONObject(0).getString("title")).isEqualTo("music1");
+         //composer 생성
+        ComposersSaveRequestDto composer1 = ComposersSaveRequestDto
+                .builder().name("composer1").build();
+        template.postForEntity(COMPOSER_ENDPOINT, composer1, ComposersResponseDto.class);
+
+        // music 생성
+        MusicsSaveRequestDto music1 = MusicsSaveRequestDto
+                .builder().title("music1").build();
+        MusicsSaveRequestDto music2 = MusicsSaveRequestDto
+                .builder().title("music2").build();
+        template.postForEntity(MUSIC_ENDPOINT, music1, MusicsResponseDto.class);
+        template.postForEntity(MUSIC_ENDPOINT, music2, MusicsResponseDto.class);
+
+        // 연관관계 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "text/uri-list");
+        HttpEntity<String> entity = new HttpEntity<>(
+                MUSIC_ENDPOINT + "/1\n" + MUSIC_ENDPOINT + "/2", headers);
+        template.exchange(COMPOSER_ENDPOINT + "/1/musics", HttpMethod.PUT, entity, String.class);
+
+        // 연관관계 생성 checking
+        String jsonResponse = template.getForObject(COMPOSER_ENDPOINT + "/1/musics", String.class);
+        System.out.println(jsonResponse);
+        JSONObject jsonObj = new JSONObject(jsonResponse).getJSONObject("_embedded");
+        JSONArray jsonArray = jsonObj.getJSONArray("musics");
+        assertThat(jsonArray.getJSONObject(0).getString("title")).isEqualTo("music1");
     }
 }
