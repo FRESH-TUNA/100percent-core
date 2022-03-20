29b4ff95c5b16d0f28d6ec00b0218b86b8b1beae
DONGWON KIM <lunacircle4@likelion.org>
2022-03-21 00:42:34 +0900

    feat: service test

--- src/test/java/com/main/koko_main_api/services/music/MusicFilterFindAllServiceTest.java
+++ src/test/java/com/main/koko_main_api/services/music/MusicFilterFindAllServiceTest.java
@@ -61,9 +61,6 @@ public class MusicFilterFindAllServiceTest {
             musics.add(music); patterns.add(pattern_hard_type); music_ids.add(i);
         }
 
-        when(patternRepository.findAllByPlayTypeAndDifficulty(
-                five_key.getId(), hard_type.getId())).thenReturn(patterns);
-
         /*
          * when
          */
@@ -76,6 +73,51 @@ public class MusicFilterFindAllServiceTest {
          */
         Page<MusicEntityToServiceDto> page = musicFilterFindAllService
                 .findAllByDifficulty(five_key.getId(), hard_type.getId());
+        assertThat(page.getSize()).isEqualTo(MUSIC_LENGTH);
+    }
+
+    @Test
+    public void 레벨_플레이타입_필터링_테스트() {
+        /*
+         * data mocking
+         */
+        //main
+        final int MUSIC_LENGTH = 2;
+        List<Music> musics = new ArrayList<>();
+        List<Pattern> patterns = new ArrayList<>();
+        List<Long> music_ids = new ArrayList<>();
+
+        //sub
+        Album album = Album.builder().title("album").build();
+        Integer level = 15;
+        PlayType five_key = PlayType.builder().title("5K").id(1L).build();
+        Long playable_id = 1L;
+
+        //init
+        for(long i = 0; i < MUSIC_LENGTH; ++i) {
+            Music music = Music.builder().album(album).id(i).build();
+
+            Pattern pattern_hard_type = Pattern.builder()
+                    .id(playable_id++).level(level)
+                    .music(music).playType(five_key).build();
+
+            music.add_playable(pattern_hard_type);
+
+            musics.add(music); patterns.add(pattern_hard_type); music_ids.add(i);
+        }
+
+        /*
+         * when
+         */
+        when(patternRepository.findAllByPlayTypeAndLevel(
+                five_key.getId(), level)).thenReturn(patterns);
+        when(musicRepository.findAllByIds(music_ids)).thenReturn(musics);
+
+        /*
+         * then
+         */
+        Page<MusicEntityToServiceDto> page = musicFilterFindAllService
+                .findAllByLevel(five_key.getId(), level);
         assertThat(page.getSize()).isEqualTo(MUSIC_LENGTH);
     }
 }
