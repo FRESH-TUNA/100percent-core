5cfdd9b38946a75701f871a8f813a0749fe344cb
DONGWON KIM <lunacircle4@likelion.org>
2022-04-25 20:09:54 +0900

    feat: Music 기능 프로토타입 개발 완료

--- src/main/java/com/main/koko_main_api/controllers/MusicController.java
+++ src/main/java/com/main/koko_main_api/controllers/MusicController.java
@@ -1,15 +1,11 @@
 package com.main.koko_main_api.controllers;
 
-import com.main.koko_main_api.domains.Music;
-import com.main.koko_main_api.dtos.music.MusicFindAllRequestParams;
 import com.main.koko_main_api.dtos.music.MusicResponseDto;
 import com.main.koko_main_api.dtos.music.MusicRequestDto;
 
 import com.main.koko_main_api.services.music.MusicService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,27 +34,24 @@ public class MusicController {
     }
 
     /*
-     * 개발중
+     * 당분간 안씀
      */
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping
-    public PagedModel<MusicResponseDto> findAll
-        (@RequestParam MusicFindAllRequestParams params, Pageable pageable) {
-
-        String mode = params.getMode();
-        Long album_id = params.getAlbum(), play_type_id = params.getPlay_type();
-        Long d_id = params.getDifficulty_type(); Integer level = params.getLevel();
-
-        if(mode == null || mode.equals("db")) {
-            if(album_id == null) return musicService.findAll(pageable, play_type_id);
-            else return musicService.findAllByAlbum(pageable, play_type_id, album_id);
-        }
-        else if(mode.equals("difficulty"))
-            return musicService.findAllByDifficulty(play_type_id, d_id);
-        else
-            return musicService.findAllByLevel(play_type_id, level);
-    }
+//    @ResponseStatus(HttpStatus.OK)
+//    @GetMapping
+//    public PagedModel<MusicResponseDto> findAll
+//        (@RequestParam MusicFindAllRequestParams params, Pageable pageable) {
+//
+//        String mode = params.getMode();
+//        Long album_id = params.getAlbum(), play_type_id = params.getPlay_type();
+//        Long d_id = params.getDifficulty_type(); Integer level = params.getLevel();
+//
+//        if(mode == null || mode.equals("db")) {
+//            if(album_id == null) return musicService.findAll(pageable, play_type_id);
+//            else return musicService.findAllByAlbum(pageable, play_type_id, album_id);
+//        }
+//        else if(mode.equals("difficulty"))
+//            return musicService.findAllByDifficulty(play_type_id, d_id);
+//        else
+//            return musicService.findAllByLevel(play_type_id, level);
+//    }
 }
-
-//        if(mode.equals("difficulty")) return difficultyFindAll(params);
-//        if(mode.equals("level")) return levelFindAll(params);
